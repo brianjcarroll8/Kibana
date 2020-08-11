4dc307ff1b7d8cd3580fafbe94f8fc3155646a9d
Thomas Heymann <190132+thomheymann@users.noreply.github.com>
2020-08-11 09:36:23 +0100

    Update x-pack/plugins/security/server/saved_objects/secure_saved_objects_client_wrapper.ts
    Co-authored-by: Larry Gregory <larry.gregory@elastic.co>

--- x-pack/plugins/security/server/saved_objects/secure_saved_objects_client_wrapper.ts
+++ x-pack/plugins/security/server/saved_objects/secure_saved_objects_client_wrapper.ts
@@ -349,7 +349,7 @@ export class SecureSavedObjectsClientWrapper implements SavedObjectsClientContra
     }
 
     auditor.add(savedObjectUpdateEvent, {
-      action: 'saved_object_delete_from_namespace',
+      action: 'saved_object_delete_from_namespaces',
       objects: [{ type, id, namespaces }],
     });
 
