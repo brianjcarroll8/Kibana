b37dc48c2eaedb3310cf263d3ab05b4bd54bd9bb
Tre' Seymour <wayne.seymour@elastic.co>
2020-08-26 13:36:53 -0600

    Thanks Lee!

---
+++ enumerate_paths.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#IFS=$'\n';
+#
+touch all_assigned.txt
+echo "" > all_assigned.txt
+#
+#for i in `cat team_assignments.txt`; do (
+#  export team=$(echo $i | cut -d' ' -f2)
+#  for file in $(find $(echo $i | cut -d' ' -f1) -type f | grep -v target |grep -v node_modules | grep -v __tests__ | grep -v __fixture__); do (
+##    echo "$file $team" >> all_assigned.txt
+#    echo "$file $team"
+#  ); done
+#); done
+#
+#
+#sed -i "" "s/\/\//\//g" all_assigned.txt
+
+IFS=$'\n'
+
+for i in `cat team_assignments.txt`; do (
+  export team=$(echo $i | cut -d' ' -f2)
+  for file in $(find $(echo $i | cut -d' ' -f1) -type f | grep -v target |grep -v node_modules | grep -v __tests__ | grep -v __fixture__); do (
+    echo "$file $team" >> all_assigned.txt
+  ); done
+); done
+
+sed -i "" "s/\/\//\//g" all_assigned.txt
